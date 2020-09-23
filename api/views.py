bd8f911e3f9f4f8c0da5a0c685e53162692c2572
Nikita Ustiuzhanin <zaebumba@gmail.com>
2020-09-23 12:48:05 +0700

    removed unused import

--- api/views.py
+++ api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
